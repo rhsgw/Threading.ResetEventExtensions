914021d48ed23cecc9ed293060de30d376b4ab7e
Ryo Hasegawa <r.hasegawa@seiko-sts.co.jp>
2020-01-25 17:54:48 +0900

    # add SignalSetter.Empty

--- SignalSetter.cs
+++ SignalSetter.cs
@@ -8,22 +8,12 @@ namespace AsyncHelper
 {
 	public readonly struct SignalSetter:IDisposable
 	{
+		public static SignalSetter Empty { get; } = new SignalSetter(default);
 		readonly AutoResetEvent resetEvent;
 		public bool Signal => resetEvent != default;
 		internal SignalSetter(AutoResetEvent autoResetEvent) =>
 			resetEvent = autoResetEvent;
 
 		public void Dispose() => resetEvent?.Set();
-	}
-	public static class AutoResetEventExtensions
-	{
-		public static ValueTask<SignalSetter> WaitOneAsync(this AutoResetEvent autoResetEvent, int timeoutMilliseconds) =>
-			autoResetEvent.WaitOneAsync(TimeSpan.FromMilliseconds(timeoutMilliseconds));
-
-		public static async ValueTask<SignalSetter> WaitOneAsync(this AutoResetEvent autoResetEvent, TimeSpan timeout) =>
-			autoResetEvent == null ? throw new ArgumentNullException(nameof(autoResetEvent)) :
-			autoResetEvent.WaitOne(0) ? new SignalSetter(autoResetEvent) :
-			await Task.Run(() => autoResetEvent.WaitOne(timeout)) ? new SignalSetter(autoResetEvent) :
-			new SignalSetter(default);
 	}
 }
