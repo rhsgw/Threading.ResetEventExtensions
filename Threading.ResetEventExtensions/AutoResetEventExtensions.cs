63c917ea107ac6683676caa1844f93cea42ad7e4
rhsgw <r-hsgw@hotmail.co.jp>
2020-02-22 18:23:29 +0900

    # update to catch exception in WaitOneAsync

--- Threading.ResetEventExtensions/AutoResetEventExtensions.cs
+++ Threading.ResetEventExtensions/AutoResetEventExtensions.cs
@@ -11,10 +11,17 @@ namespace Threading.ResetEventExtensions
 		public static ValueTask<WaitedEvent> WaitOneAsync(this AutoResetEvent autoResetEvent, int timeoutMilliseconds) =>
 			autoResetEvent.WaitOneAsync(TimeSpan.FromMilliseconds(timeoutMilliseconds));
 
-		public static async ValueTask<WaitedEvent> WaitOneAsync(this AutoResetEvent autoResetEvent, TimeSpan timeout) =>
-			autoResetEvent == null ? throw new ArgumentNullException(nameof(autoResetEvent)) :
-			autoResetEvent.WaitOne(0) ? new WaitedEvent(autoResetEvent) :
-			await Task.Run(() => autoResetEvent.WaitOne(timeout)) ? new WaitedEvent(autoResetEvent) :
-			WaitedEvent.Empty;
+		public static async ValueTask<WaitedEvent> WaitOneAsync(this AutoResetEvent autoResetEvent, TimeSpan timeout)
+		{
+			if(autoResetEvent == null) throw new ArgumentNullException(nameof(autoResetEvent));
+			try
+			{
+				return
+					autoResetEvent.WaitOne(0) ? new WaitedEvent(autoResetEvent) :
+					await Task.Run(() => autoResetEvent.WaitOne(timeout)) ? new WaitedEvent(autoResetEvent) :
+					WaitedEvent.Empty;
+			}
+			catch { return WaitedEvent.Empty; }
+		}
 	}
 }
